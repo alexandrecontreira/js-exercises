7cb84b24cbd71c62625e83d74d2b52a92cbbdf0e
Alexandre <contreira1106@gmail.com>
2024-03-03 19:01:31 -0300

    exercicios alunos

---
+++ jogo_num/jogo_num_v1.js
@@ -0,0 +1,7 @@
+let numSec = parseInt(Math.random() * 10 + 1);
+let numDig = prompt('Digite um numero entre 1 e 10:');
+if (numSec === numDig) {
+    prompt('Parabens você acertou!')
+} else {
+    prompt('Que pena, tente novamente!')
+}
---
+++ jogo_num/jogo_num_v2.js
@@ -0,0 +1,14 @@
+let numSec = parseInt(Math.random() * 10 + 1);
+let numDig = 0;
+while (numSec != numDig) {
+    numDig = prompt('Digite um numero entre 1 e 10:');
+
+    prompt('Que pena, tente novamente!')
+
+}
+if (numSec == numDig) {
+    prompt('Parabens você acertou!')
+
+
+
+

e76fcd526a147ffaf9a4fc5166251049ff53c817
Alexandre Contreira <77118607+alexandrecontreira@users.noreply.github.com>
2024-03-05 21:12:07 -0300

    melhorias no codigo

--- exercise-3.js
+++ exercise-3.js
@@ -7,3 +7,19 @@ let soma = 0;
 }
  let media = soma/4
 console.log(media.toFixed(1).replace(',','.'))
+
+
+// let i = 0;
+// let soma = 0;
+//  while (i <= 3){
+//      let nota = parseFloat(prompt('digite a nota:').replace(',','.'));
+//      soma = soma + nota;
+//      i++;
+// }
+//  let media = soma/4
+//  console.log(media.toFixed(1).replace(',','.'))
+//  if(media >= 6.0){
+//      prompt("Parabens,você está aprovado")
+//  }else{
+//      prompt("Que pena!Você está reprovado")
+//  }

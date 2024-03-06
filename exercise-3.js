d884c2a5aab8db4b63c9690c4893ac56ec5b1ae5
Alexandre Contreira <77118607+alexandrecontreira@users.noreply.github.com>
2024-03-05 21:06:37 -0300

    melhoria no codigo

--- exercise-3.js
+++ exercise-3.js
@@ -1,10 +1,9 @@
-let soma = 0;
-
 let i = 0;
- while (i < 3){
+let soma = 0;
+ while (i <= 3){
      let nota = parseFloat(prompt('digite a nota:').replace(',','.'));
-     soma += nota;
+     soma = soma + nota;
      i++;
- }
-let media = soma/4
-console.log(media.toFixed)
+}
+ let media = soma/4
+console.log(media.toFixed(1).replace(',','.'))
